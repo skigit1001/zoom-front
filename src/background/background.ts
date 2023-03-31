ec21efefdce3f7090b5be38eb12f0fb915cc1871
devofficer <sompaqueen47@gmail.com>
2023-03-31 15:27:40 -0700

    fix: remove console.log

--- src/background/background.ts
+++ src/background/background.ts
@@ -2,7 +2,6 @@ import ZoomRTMessages from "@/utils/enums/ZoomRTMessages";
 import './capture';
 
 chrome.runtime.onMessage.addListener((message) => {
-  console.log(message);
   if (message.type === ZoomRTMessages.NewMessage) {
     console.log(message.data)
   }
