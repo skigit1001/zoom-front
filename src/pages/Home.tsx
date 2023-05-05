dcc23817425e7c4917d1eaed89db6c5300bc6aad
devofficer <sompaqueen47@gmail.com>
2023-05-05 03:59:10 -0700

    chore: rename Record to Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
+export default function Home() {
+  const handleRequest = () => {
+    chrome.tabs.query({ active: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type: "camera-request"
+      });
+    });
+
+    // Check if recording is ongoing
+    chrome.runtime.sendMessage({ type: "record-request" });
+  }
+
+  const handleRecord = () => {
+    chrome.runtime.sendMessage({ type: "record" });
+  };
+
+  const handleStopRecord = () => {
+    chrome.runtime.sendMessage({ type: "stop-save" });
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Box display="flex" justifyContent="center" alignItems="center" mt={2} mb={4}>
+        <img src="/logo.png" width={150} />
+      </Box>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={handleRequest}
+      >
+        Request
+      </Button>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={handleRecord}
+      >
+        Record
+      </Button>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={handleStopRecord}
+      >
+        Stop Record
+      </Button>
+    </Box>
+  );
+}
