64c08070fc2cbb994bc36a55f2494d3a3d39864e
devofficer <sompaqueen47@gmail.com>
2023-06-08 21:17:50 -0400

    fix: replace sign up api with baseAPI

--- src/popup/pages/SignUp.tsx
+++ src/popup/pages/SignUp.tsx
@@ -7,9 +7,9 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { PopupPages } from '@/utils/enums/PopupPages';
-import useAPI from '@/hooks/useAPI';
 import { StorageItems } from '@/utils/enums/StorageItems';
 import { useStorage } from '@/hooks/useStorage';
+import baseApi from '@/services/baseApi';
 
 enum SignUpItems {
   Username = 'name',
@@ -20,7 +20,6 @@ enum SignUpItems {
 
 export default function SignUp() {
   const navigate = useNavigate();
-  const authAPI = useAPI('auth');
   const [, setAuthToken] = useStorage(StorageItems.AuthToken);
   const [, setUserInfo] = useStorage(StorageItems.UserInfo);
 
@@ -43,14 +42,14 @@ export default function SignUp() {
 
   const handleSubmit = React.useCallback(async () => {
     try {
-      const { data } = await authAPI.post('/signup', formData);
+      const { data } = await baseApi.post('/auth/signup', formData);
       setAuthToken(data.token);
       setUserInfo(data.user);
       setTimeout(() => navigate(PopupPages.home));
     } catch (err) {
       console.error(err);
     }
-  }, [formData, authAPI]);
+  }, [formData]);
 
   return (
     <Box
